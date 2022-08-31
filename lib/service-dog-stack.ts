2d534dadf60ae88dc8ec44e41964941b637b0594
Denise Jarry <djarry@atlassian.com>
2022-08-31 15:31:39 +1000

    names functions and gives invoke and start stop instance permissions

--- lib/service-dog-stack.ts
+++ lib/service-dog-stack.ts
@@ -4,12 +4,14 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class ServiceDogStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        new NodejsFunction(this, 'appCommands', {
+        const appCommandsFunction = new NodejsFunction(this, 'appCommands', {
+            functionName: 'appCommands',
             runtime: lambda.Runtime.NODEJS_16_X,
             entry: join('resources', 'appCommands', 'index.ts'),
             environment: {
@@ -17,15 +19,21 @@ export class ServiceDogStack extends Stack {
             },
         });
 
-        new NodejsFunction(this, 'minecraftServer', {
-            runtime: lambda.Runtime.NODEJS_16_X,
-            entry: join('resources', 'minecraftServer', 'index.ts'),
-            environment: {
-                INSTANCE_ID: process.env.INSTANCE_ID || '',
-            },
-        });
+        const minecraftServerFunction = new NodejsFunction(
+            this,
+            'minecraftServer',
+            {
+                functionName: 'minecraftServer',
+                runtime: lambda.Runtime.NODEJS_16_X,
+                entry: join('resources', 'minecraftServer', 'index.ts'),
+                environment: {
+                    INSTANCE_ID: process.env.INSTANCE_ID || '',
+                },
+            }
+        );
 
         new NodejsFunction(this, 'postIpAddress', {
+            functionName: 'postIpAddress',
             runtime: lambda.Runtime.NODEJS_16_X,
             entry: join('resources', 'postIpAddress', 'index.ts'),
             environment: {
@@ -33,5 +41,21 @@ export class ServiceDogStack extends Stack {
                 DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN || '',
             },
         });
+
+        appCommandsFunction.addToRolePolicy(
+            new PolicyStatement({
+                actions: ['lambda:InvokeFunction', 'lambda:InvokeAsync'],
+                resources: [minecraftServerFunction.functionArn],
+            })
+        );
+
+        minecraftServerFunction.addToRolePolicy(
+            new PolicyStatement({
+                actions: ['ec2:StartInstances', 'ec2:StopInstances'],
+                resources: [
+                    `arn:aws:ec2:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:instance/${process.env.INSTANCE_ID}`,
+                ],
+            })
+        );
     }
 }
