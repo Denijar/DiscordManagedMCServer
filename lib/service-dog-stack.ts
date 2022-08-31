755e775fd7a6bcb0ff9fb597f049c867c95caafc
Denise Jarry <djarry@atlassian.com>
2022-08-31 17:36:17 +1000

    eventbridge rule and postIpAddress permissions

--- lib/service-dog-stack.ts
+++ lib/service-dog-stack.ts
@@ -5,12 +5,17 @@ import { Construct } from 'constructs';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Rule } from 'aws-cdk-lib/aws-events';
+import {
+    addLambdaPermission,
+    LambdaFunction,
+} from 'aws-cdk-lib/aws-events-targets';
 
 export class ServiceDogStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const appCommandsFunction = new NodejsFunction(this, 'appCommands', {
+        const appCommands = new NodejsFunction(this, 'appCommands', {
             functionName: 'appCommands',
             runtime: lambda.Runtime.NODEJS_16_X,
             entry: join('resources', 'appCommands', 'index.ts'),
@@ -19,20 +24,16 @@ export class ServiceDogStack extends Stack {
             },
         });
 
-        const minecraftServerFunction = new NodejsFunction(
-            this,
-            'minecraftServer',
-            {
-                functionName: 'minecraftServer',
-                runtime: lambda.Runtime.NODEJS_16_X,
-                entry: join('resources', 'minecraftServer', 'index.ts'),
-                environment: {
-                    INSTANCE_ID: process.env.INSTANCE_ID || '',
-                },
-            }
-        );
+        const minecraftServer = new NodejsFunction(this, 'minecraftServer', {
+            functionName: 'minecraftServer',
+            runtime: lambda.Runtime.NODEJS_16_X,
+            entry: join('resources', 'minecraftServer', 'index.ts'),
+            environment: {
+                INSTANCE_ID: process.env.INSTANCE_ID || '',
+            },
+        });
 
-        new NodejsFunction(this, 'postIpAddress', {
+        const postIpAddress = new NodejsFunction(this, 'postIpAddress', {
             functionName: 'postIpAddress',
             runtime: lambda.Runtime.NODEJS_16_X,
             entry: join('resources', 'postIpAddress', 'index.ts'),
@@ -42,14 +43,14 @@ export class ServiceDogStack extends Stack {
             },
         });
 
-        appCommandsFunction.addToRolePolicy(
+        appCommands.addToRolePolicy(
             new PolicyStatement({
                 actions: ['lambda:InvokeFunction', 'lambda:InvokeAsync'],
-                resources: [minecraftServerFunction.functionArn],
+                resources: [minecraftServer.functionArn],
             })
         );
 
-        minecraftServerFunction.addToRolePolicy(
+        minecraftServer.addToRolePolicy(
             new PolicyStatement({
                 actions: ['ec2:StartInstances', 'ec2:StopInstances'],
                 resources: [
@@ -57,5 +58,26 @@ export class ServiceDogStack extends Stack {
                 ],
             })
         );
+
+        postIpAddress.addToRolePolicy(
+            new PolicyStatement({
+                actions: ['ec2:DescribeInstances'],
+                resources: ['*'],
+            })
+        );
+
+        const EC2RunningRule = new Rule(this, 'EC2Running', {
+            ruleName: 'EC2Running',
+            eventPattern: {
+                source: ['aws.ec2'],
+                detailType: ['EC2 Instance State-change Notification'],
+                detail: {
+                    state: ['running'],
+                    'instance-id': [process.env.INSTANCE_ID],
+                },
+            },
+        });
+
+        EC2RunningRule.addTarget(new LambdaFunction(postIpAddress));
     }
 }
